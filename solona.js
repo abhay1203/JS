59ff82fb41366be79db31398750fed2ac8d84f15
Abhay <abhayrandhe530@gmail.com>
2022-12-02 10:08:50 +0000

    Create 5 files

---
+++ solona.js
@@ -0,0 +1,7 @@
+console.log("Abhay is Coding");
+console.log(typeof 23456);
+console.log(typeof 238646843546546);
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof undefined);
+console.log(typeof null);
